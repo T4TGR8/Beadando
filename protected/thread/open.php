e9a39ef68c25109775e5ae6963736ca994b30403
Barna Bence <barnabence8@gmail.com>
2020-04-22 20:46:02 +0200

    Comment system added, Users can be deleted by admin

--- protected/thread/open.php
+++ protected/thread/open.php
@@ -1,15 +1,109 @@
 <?php 
+	if(array_key_exists('d', $_GET) && !empty($_GET['d'])) {
+		$query = "DELETE FROM comments WHERE id = :id";
+		$params = [':id' => $_GET['d']];
+		require_once DATABASE_CONTROLLER;
+		if(!executeDML($query, $params)) {
+			echo "Hiba törlés közben!";
+		}
+	}
+?>
+<?php
 	if(array_key_exists('o', $_GET) && !empty($_GET['o'])) {
-		$query = "SELECT title, ftext FROM threads WHERE id = :id";
+		$query = "SELECT id, title, ftext FROM threads WHERE id = :id";
 		$params = [':id' => $_GET['o']];
 		require_once DATABASE_CONTROLLER;
 		$openthread = getRecord($query, $params);
 	}
-	else {
-		echo "kacsa";
+	if(array_key_exists('o', $_GET) && !empty($_GET['o'])) {
+		$query = "SELECT id, ctext, author, comcreated FROM comments WHERE threadid = :id";
+		$params = [':id' => $_GET['o']];
+			require_once DATABASE_CONTROLLER;
+		$comments = getList($query, $params);
 	}
-	
-	echo "<h2>".$openthread['title']."</h2>";
-	echo "<br>";
-	echo nl2br($openthread['ftext']);
+	else {
+		echo "Hiba!";
+	}
 ?>
+
+<?php if(!isset($_SESSION['permission'])) : ?>
+	<table class = "table">
+			<thead>
+				<tr>
+					<th scope="col"><?php echo "<h2>".$openthread['title']."</h2>" ?></th>
+				</tr>
+			</thead>
+				<tbody>
+					<tr>
+						<td><?php echo nl2br($openthread['ftext']) ?></td>
+					</tr>
+						<?php if(count($comments) > 0) : ?>
+						<?php $i = 0; ?>
+						<?php foreach ($comments as $c) : ?>
+						<?php $i++; ?>
+						<tr>
+							<td><?=$c['ctext'] ?><?php echo "<br>"; ?><b><?=$c['author'] ?><?php echo " "; ?><?=$c['comcreated'] ?></b></td>
+						</tr>
+					<?php endforeach;?>
+				<?php endif; ?>
+				</tbody>
+		</table>
+	<?php else : ?>
+	<table class = "table">
+			<thead>
+				<tr>
+					<th scope="col"><?php echo "<h2>".$openthread['title']."</h2>" ?></th>
+					<th></th>
+				</tr>
+			</thead>
+				<tbody>
+					<tr>
+						<td><?php echo nl2br($openthread['ftext']) ?></td>
+					</tr>
+					<?php if(count($comments) > 0) : ?>
+					<?php $i = 0; ?>
+					<?php foreach ($comments as $c) : ?>
+						<?php $i++; ?>
+						<tr>
+							<td><?=$c['ctext'] ?><?php echo "<br>"; ?><b><?=$c['author'] ?><?php echo " "; ?><?=$c['comcreated'] ?></b></td>
+							<?php if($_SESSION['permission'] > 0) : ?>
+							<td align='right'><a href="index.php?P=open_thread&o=<?=$openthread['id'] ?>&d=<?=$c['id'] ?>">&#128465</a></td>
+							<?php endif; ?>
+						</tr>
+					<?php endforeach;?>
+				<?php endif; ?>
+				</tbody>
+	</table>
+	<?php
+		if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['commentThread'])) {
+			$postData = [
+				'ctext' => $_POST['ctext']
+			];
+
+			if(empty($postData['ctext'])) {
+				echo "Hiányzó adat(ok)!";
+			}
+			else {
+				$query = "INSERT INTO comments (ctext, author, comcreated, threadid) VALUES (:ctext, :author, :comcreated, :threadid)";
+				$params = [
+					':ctext' => $postData['ctext'],
+					':author' => $_SESSION['uname'],
+					':comcreated' => date("Y-m-d H:i:s"),
+					':threadid' => $openthread['id']
+				];
+				require_once DATABASE_CONTROLLER;
+				if(!executeDML($query, $params)) {
+					echo "Hiba az adatbevitel során!";
+				} echo "<meta http-equiv='refresh' content='0'>";
+			}
+		}
+		?>
+
+		<form method="post">
+	  		<div class="form-group">
+	    		<label for="commentText">Your Comment</label>
+	    		<textarea class="form-control" id="commentText" name="ctext" rows="5"></textarea>
+	  		</div>
+			<button type="submit" class="btn btn-primary" name="commentThread">Comment</button>
+		</form>
+	<?php endif; ?>
--- protected/user/user_list.php
+++ protected/user/user_list.php
@@ -1,8 +1,18 @@
 <?php if(!isset($_SESSION['permission']) || $_SESSION['permission'] < 1) : ?>
 	<h1>Page access is forbidden!</h1>
 <?php else : ?>
+<?php 
+	if(array_key_exists('d', $_GET) && !empty($_GET['d'])) {
+		$query = "DELETE FROM users WHERE id = :id";
+		$params = [':id' => $_GET['d']];
+		require_once DATABASE_CONTROLLER;
+		if(!executeDML($query, $params)) {
+			echo "Hiba törlés közben!";
+		}
+	}
+?>
 	<?php 
-	$query = "SELECT username, email, permission FROM users";
+	$query = "SELECT id, username, email, permission FROM users";
 	require_once DATABASE_CONTROLLER;
 	$users = getList($query);
 	?>
@@ -16,6 +26,7 @@
 					<th scope="col">Username</th>
 					<th scope="col">Email</th>
 					<th scope="col">Permission</th>
+					<th></th>
 				</tr>
 			</thead>
 			<tbody>
@@ -27,6 +38,7 @@
 						<td><?=$u['username'] ?></td>
 						<td><?=$u['email'] ?></td>
 						<td><?=$u['permission'] ?></td>
+						<td align='right'><a href="index.php?P=users&d=<?=$u['id'] ?>">&#128465</a></td>
 					</tr>
 				<?php endforeach;?>
 			</tbody>
